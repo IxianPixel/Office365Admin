a7163d68fbe98976efa446383d4f06ec9bc9cdb3
IxianPixel <dylanaddison@gmail.com>
2015-07-21 13:49:04 +0100

    Added existing module code to repo

---
+++ Office365Admin.psd1
@@ -0,0 +1,38 @@
+﻿@{
+	## Module Info
+	ModuleVersion      = '0.0.0.1'
+	Description        = 'Office365Admin Module'
+	GUID               = '77678c53-a074-4995-a829-fc82b75a09a3'
+
+	## Module Components
+	ScriptsToProcess   = @()
+	ModuleToProcess    = @("Office365Admin.psm1")
+	TypesToProcess     = @()
+	FormatsToProcess   = @()
+	ModuleList         = @("Office365Admin.psm1")
+	FileList           = @()
+
+	## Public Interface
+	CmdletsToExport    = ''
+	FunctionsToExport  = '*'
+	VariablesToExport  = '*'
+	AliasesToExport    = '*'
+
+	## Requirements
+	PowerShellVersion      = '3.0'
+	PowerShellHostName     = ''
+	PowerShellHostVersion  = ''
+	RequiredModules        = @()
+	RequiredAssemblies     = @()
+	ProcessorArchitecture  = 'None'
+	DotNetFrameworkVersion = '2.0'
+	CLRVersion             = '2.0'
+
+	## Author
+	Author             = 'Dylan Addison'
+	CompanyName        = 'Wavehill'
+	Copyright          = ''
+
+	## Private Data
+	PrivateData        = ''
+}
---
+++ Office365Admin.psm1
@@ -0,0 +1,179 @@
+﻿Function Connect-Tenant()
+{
+    <#
+    .SYNOPSIS
+    Connects to an Office 365 tenant.
+
+    .DESCRIPTION
+    Connects to an Office 365 MSOL and Office 365 ECP for a specific tenant. Credentials must be provided to authenticate against the right tenant.
+    #>
+    Param(
+            [parameter(Mandatory=$true)]
+            [PSCredential]
+            $Credential
+        )
+    
+    Write-Progress -Activity "Connecting to Tenant" -Status "Connecting to ECP" -Id 506 -PercentComplete 25
+    $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection
+    Import-Module (Import-PSSession $session) -Global
+
+    Write-Progress -Activity "Connecting to Tenant" -Status "Connecting to MSOL" -Id 506 -PercentComplete 75
+    Connect-MsolService -Credential $Credential
+
+    Write-Progress -Activity "Connecting to Tenant" -Status "Connected" -Id 506 -PercentComplete 100
+
+    $companyName = Get-MsolPartnerInformation | Select-Object PartnerCompanyName
+    [Console]::Title = $companyName.PartnerCompanyName
+}
+
+Function Connect-ECP()
+{
+    <#
+    .SYNOPSIS
+    Connects to an Office 365 ECP.
+
+    .DESCRIPTION
+    Connects to an Office 365 ECP for a specific tenant. Credentials must be provided to authenticate against the right tenant. 
+    This will only provide functions for managing the ECP not the MSOL. Use Connect-Tenant to access both MSOL and ECP functions.
+    #>
+    Param(
+            [parameter(Mandatory=$true)]
+            [PSCredential]
+            $Credential
+        )
+
+    $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection
+    Import-Module (Import-PSSession $session) -Global
+}
+
+Function Get-TenantLicenseSummary()
+{
+    <#
+    .SYNOPSIS
+    Gets a summary of licenses for all tenants.
+
+    .DESCRIPTION
+    Shows a list of all tenants with each license type, number purchased and number consumed.
+    Must be run whilst connected to MSOL as a user with partner access. 
+    #>
+    $licenses = @()
+    $tenants = Get-MsolPartnerContract | Select-Object Name, TenantId
+
+    ForEach($tenant in $tenants)
+    {
+        $skus = Get-MsolAccountSku -TenantId $tenant.TenantId
+
+        ForEach($sku in $skus)
+        {
+            $license = New-Object -TypeName PSObject
+
+            Add-Member -InputObject $license -MemberType NoteProperty -Name Name -Value $tenant.Name
+            Add-Member -InputObject $license -MemberType NoteProperty -Name AccountSku -Value $sku.AccountSkuId
+            Add-Member -InputObject $license -MemberType NoteProperty -Name ActiveUnits -Value $sku.ActiveUnits
+            Add-Member -InputObject $license -MemberType NoteProperty -Name WarningUnits -Value $sku.WarningUnits
+            Add-Member -InputObject $license -MemberType NoteProperty -Name ConsumedUnits -Value $sku.ConsumedUnits
+
+            $licenses = $licenses + $license
+        }
+    }
+
+    return $licenses
+}
+
+Function Get-UserDelegatePermission()
+{
+    <#
+    .SYNOPSIS
+    Gets a list of mailboxes the specified user has permission to.
+
+    .DESCRIPTION
+    Searches through all mailboxes and finds which mailboxes the specified user has access to.
+    Must be connected to an ECP for this to work. 
+    #>
+    Param(
+        [parameter(Mandatory=$true)]
+        [String]
+        $User
+    )
+
+    $users = @()
+
+    Write-Progress -Activity "Searching Mailboxes" -Status "Getting Mailboxes" -PercentComplete 0
+    $mailboxes = Get-Mailbox
+    $iterator = 1
+
+    ForEach($mailbox in $mailboxes)
+    {
+        $users = $users + ($mailbox | Get-MailboxPermission | Where-Object { $_.User -like "*$User*" })
+        
+        Write-Progress -Activity "Searching Mailboxes" -Status "Mailbox [$iterator/$($mailboxes.Count)]: $mailbox" -PercentComplete (($iterator/$mailboxes.Count) * 100)
+        $iterator++
+    }
+
+    return $users
+}
+
+Function Convert-MailboxToSharedMailbox()
+{
+    Param(
+        [parameter(Mandatory=$true)]
+        [String]
+        $Identity
+    )
+
+    Write-Progress -Activity "Convert Mailbox to Shared Mailbox" -Status "Converting Mailbox" -Id 507 -PercentComplete 33
+    Set-Mailbox $Identity -Type shared -ProhibitSendReceiveQuota 10GB -ProhibitSendQuota 9.5GB -IssueWarningQuota 9GB
+
+    Write-Progress -Activity "Convert Mailbox to Shared Mailbox" -Status "Getting existing license" -Id 507 -PercentComplete 66
+    $license = Get-MsolUser -UserPrincipalName $Identity | Select-Object -ExpandProperty Licenses
+
+    Write-Progress -Activity "Convert Mailbox to Shared Mailbox" -Status "Removing license" -Id 507 -PercentComplete 99
+    Set-MsolUserLicense -UserPrincipalName $Identity -RemoveLicenses $license.AccountSkuId
+}
+
+Function New-BulkMsolUser()
+{
+    Param(
+        [parameter(Mandatory=$true, Position=1)]
+        [Object[]]
+        $Users,
+
+        [Parameter(Position=2)]
+        [String]
+        $TenantId = "",
+
+        [Parameter(Position=3)]
+        [Boolean]
+        $ForcePasswordChange = $false,
+
+        [Parameter(Position=4)]
+        [Boolean]
+        $PasswordNeverExpires = $true
+    )
+
+    ForEach ($user in $Users)
+    {
+        If ($TenantId)
+        {
+            New-MsolUser -DisplayName $user.DisplayName -UserPrincipalName $user.UserPrincipalName -FirstName $user.FirstName -LastName $user.LastName -LicenseAssignment $user.LicenseAssignment -UsageLocation $user.UsageLocation -PasswordNeverExpires $PasswordNeverExpires -TenantId $TenantId
+
+            Set-MsolUserPassword -UserPrincipalName $user.UserPrincipalName -NewPassword $user.PasswordString -ForceChangePassword $ForcePasswordChange -TenantId $TenantId
+
+            Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName -AddLicenses $user.LicenseAssignment -TenantId $TenantId
+        }
+        Else
+        {
+            New-MsolUser -DisplayName $user.DisplayName -UserPrincipalName $user.UserPrincipalName -FirstName $user.FirstName -LastName $user.LastName -UsageLocation $user.UsageLocation -PasswordNeverExpires $PasswordNeverExpires
+
+            Set-MsolUserPassword -UserPrincipalName $user.UserPrincipalName -NewPassword $user.PasswordString -ForceChangePassword $ForcePasswordChange
+
+            Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName -AddLicenses $user.LicenseAssignment 
+        }
+    }
+}
+
+# Set Aliases
+Set-Alias cctn Connect-Tenant -Scope Global
+Set-Alias ccecp Connect-ECP -Scope Global
+# TODO: Add Alias for Get-TenantLicenseSummary
+# TODO: Add Alias for Get-UserDelegatePermission
